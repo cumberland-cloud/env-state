f4ea4ab1ed454841a834fba5c9a3311603095598
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-28 17:43:33 -0400

    init gh pages

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  default_tags    = {
-    Project       = "Cumberland Cloud"
-    Environment   = "Development"
-    Owner         = "Grant Moore"
-    Contact       = "chinchalinchin@gmail.com"
-  }
-}
