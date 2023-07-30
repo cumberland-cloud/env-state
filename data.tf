2c58ce2af7fd1467e466e0bdf78f8859631549a7
Grant Moore <chinchalinchin@gmail.com>
2023-07-30 16:41:24 -0400

    set up policy for bucket

--- data.tf
+++ data.tf
@@ -1,1 +1,32 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "platform" { # TODO: policy for tenant access
+    statement {
+        sid             = "TFStatePerms"
+        effect          = "Allow"
+        actions         = [
+            "s3:Get*",
+            "s3:List*"
+        ]
+        resources       = local.bucket_arns
+
+        principals {
+            type        =  "AWS"
+            identifiers = [
+                "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            ]
+        }
+    }
+
+    statement {
+        sid         = "DenyACLActions"
+        effect      = "Deny"
+        actions     = [
+            "s3:Delete*",
+            "s3:PutBucketAcl",
+            "s3:PutObjectAcl",
+            "s3:PutObjectVersionAcl"
+        ]
+        resources   = local.bucket_arns
+  }
+}
